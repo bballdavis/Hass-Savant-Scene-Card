45be468e7fc4b24f425c1f38fa6b81dfebb62b0d
bballdavis <bballdavis@hotmail.com>
2025-05-13 20:12:06 -0500

    quick update to verify were loading the right one

--- savant-energy-scenes-card.js
+++ savant-energy-scenes-card.js
@@ -2,7 +2,7 @@
 
 // Register the card in the customCards array - important for Home Assistant to discover the card
 console.info(
-  "%c SAVANT-ENERGY-SCENES-STANDALONE-CARD %c v1.1.7 ",
+  "%c SAVANT-ENERGY-SCENES-STANDALONE-CARD %c v1.1.9 ",
   "color: white; background: #4CAF50; font-weight: 700;",
   "color: #4CAF50; background: white; font-weight: 700;"
 );
